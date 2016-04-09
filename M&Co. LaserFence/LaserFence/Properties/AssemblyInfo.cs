539d878f359791999e4a7fa2408c2827de3096a4
Rikiki <renaud.gertz@gmail.com>
2016-04-09 23:03:47 +0200

    Updated to A13. Added an interraction cell to avoid cases where the player orders to activate the fence but the pawn is standing in front of the activated direction.

--- M&Co. LaserFence/LaserFence/Properties/AssemblyInfo.cs
+++ M&Co. LaserFence/LaserFence/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("M&Co. Common")]
+[assembly: AssemblyTitle("Deepdriller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Deepdriller")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
