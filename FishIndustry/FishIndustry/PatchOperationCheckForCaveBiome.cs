d31784d253370e78f5908591cc3f89eb6632ba9c
Rikiki <renaud.gertz@gmail.com>
2017-06-18 21:51:38 +0200

    Renamed patch file.

--- FishIndustry/FishIndustry/PatchOperationCheckForCaveBiome.cs
+++ FishIndustry/FishIndustry/PatchOperationCheckForCaveBiome.cs
@@ -8,27 +8,22 @@ using UnityEngine;   // Always needed
 using RimWorld;      // RimWorld specific functions are found here
 using Verse;         // RimWorld universal objects are here
 using Verse.AI;    // Needed when you do something with the AI
+using System.Xml;
 //using Verse.Sound; // Needed when you do something with the Sound
 
 namespace FishIndustry
 {
     /// <summary>
-    /// Item_Oyster class.
+    /// PatchOperationCheckForCaveBiome class.
     /// </summary>
     /// <author>Rikiki</author>
     /// <permission>Use this code as you want, just remember to add a link to the corresponding Ludeon forum mod release thread.
     /// Remember learning is always better than just copy/paste...</permission>
-    class Item_Oyster : ThingWithComps
+    class PatchOperationCheckForCaveBiome : PatchOperation
     {
-        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        protected override bool ApplyWorker(XmlDocument xml)
         {
-            // Small chance to spawn a pearl.
-            if (Rand.Value < 0.02f)
-            {
-                GenSpawn.Spawn(Util_FishIndustry.PearlDef, this.Position, this.MapHeld);
-            }
-            base.Destroy(mode);
+            return ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name == "CaveBiome");
         }
-
     }
 }
