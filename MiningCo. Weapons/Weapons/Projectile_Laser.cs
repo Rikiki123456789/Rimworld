70a635f8e20fcfda275fdd4d957f0d23257611f8
Rikiki <renaud.gertz@gmail.com>
2019-11-05 17:11:58 +0100

    Rewrote laser Projectile_Laser class to use vanilla code. Added a Projectile_LaserDrawer class only responsible of drawing.

--- MiningCo. Weapons/Weapons/Projectile_Laser.cs
+++ MiningCo. Weapons/Weapons/Projectile_Laser.cs
@@ -17,292 +17,17 @@ namespace Weapons
     /// </summary>
     /// <author>Rikiki</author>
     /// <permission>Use this code as you want, just remember to add a link to the corresponding Ludeon forum mod release thread.</permission>
-    public class Projectile_Laser : Bullet
+    public class Projectile_Laser : Projectile
     {
-        // TODO: check Impact is called.
-        // TODO: convert sounds to ogg.
-        // TODO: check save/load.
-        // TODO: check out of bounds.
-
-        // Variables.
-        public int tickCounter = 0;
-
-        ThingDef_LaserProjectile additionalParameters
-        {
-            get
-            {
-                return def as ThingDef_LaserProjectile;
-            }
-        }
-
-        // Draw variables.
-        public const float preFiringInitialIntensity = 0f;
-        public const float preFiringFinalIntensity = 1f;
-        public const float postFiringInitialIntensity = 1f;
-        public const float postFiringFinalIntensity = 0f;
-        public Matrix4x4 drawingMatrix = default(Matrix4x4);
-        public Vector3 drawingScale;
-        public Vector3 drawingPosition;
-        public float drawingIntensity = 0f;
-        public Material drawingTexture = null;
-
-        // ===================== Setup Work =====================
-        public override void SpawnSetup(Map map, bool respawningAfterLoad)
-        {
-            base.SpawnSetup(map, respawningAfterLoad);
-            Log.Message("SpawnSetup");
-            Log.Message("additionalParameters.preFiringDuration = " + additionalParameters.preFiringDuration);
-            Log.Message("additionalParameters.postFiringDuration = " + additionalParameters.postFiringDuration);
-
-        }
-
-        /// <summary>
-        /// Save and load internal state variables (stored in savegame data).
-        /// </summary>
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Values.Look<int>(ref this.tickCounter, "tickCounter", 0);
-
-            /*if (Scribe.mode == LoadSaveMode.PostLoadInit)
-            {
-                GetParametersFromXml();
-            }*/
-        }
-
         // ===================== Main Work Function =====================
         /// <summary>
-        /// Main projectile sequence.
+        /// Check for interception on whole trajectory in one tick then spawn projectile drawer.
         /// </summary>
         public override void Tick()
         {
-            /*base.Tick();
-            Log.Message("this.HitFlags = " + this.HitFlags);*/
-            
-            // TODO: check it is not called when loading a savegame with existing lasers.
-            if (this.tickCounter == 0)
+            while (this.DestroyedOrNull() == false)
             {
-                PerformPreFiringTreatment();
-            }
-
-            if (this.tickCounter == this.additionalParameters.preFiringDuration)
-            {
-                Log.Message("TODO: ApplyDamage");
-            }
-            ComputeDrawingParameters();
-
-            if (this.tickCounter == (this.additionalParameters.preFiringDuration + this.additionalParameters.postFiringDuration))
-            {
-                this.Destroy(DestroyMode.Vanish);
-            }
-
-            /*if (this.launcher is Pawn)
-            {
-                Pawn launcherPawn = this.launcher as Pawn;
-                if (((launcherPawn.stances.curStance is Stance_Warmup) == false)
-                    && ((launcherPawn.stances.curStance is Stance_Cooldown) == false))
-                {
-                    this.Destroy(DestroyMode.Vanish);
-                }
-            }*/
-
-            this.tickCounter++;
-        }
-
-        /// <summary>
-        /// Determines impact position and initialize drawing parameters.
-        /// </summary>
-        public virtual void PerformPreFiringTreatment()
-        {
-            // We use grounded origin position to get rid of y value in magnitude calculation.
-            this.origin.y = 0f;
-            DetermineImpactExactPosition();
-            Vector3 unitVector = (this.destination - this.origin).normalized;
-            Vector3 laserRayVector = this.destination - this.origin;
-            Vector3 cannonMouthOffset = laserRayVector.normalized * 0.9f;
-            this.drawingScale = new Vector3(1f, 1f, laserRayVector.magnitude - cannonMouthOffset.magnitude);
-            this.drawingPosition = this.origin + cannonMouthOffset + (laserRayVector - cannonMouthOffset) * 0.5f + Vector3.up * this.def.Altitude;
-            this.drawingMatrix.SetTRS(this.drawingPosition, laserRayVector.AngleFlat().ToQuat(), this.drawingScale);
-            this.Position = (this.origin + laserRayVector * 0.5f).ToIntVec3();
-            // TODO: check out of bounds case.
-
-            MoteMaker.ThrowMicroSparks(this.destination, this.Map);
-
-
-            /*Log.Message("this.origin = " + this.origin);
-            Log.Message("originGrounded = " + originGrounded);
-            Log.Message("this.destination = " + this.destination);
-            Log.Message("unitVector = " + unitVector);
-            Log.Message("laserRayVector = " + laserRayVector);
-            Log.Message("laserRayVector.magnitude = " + laserRayVector.magnitude);
-            Log.Message("this.drawingScale = " + this.drawingScale);
-            Log.Message("this.drawingPosition = " + this.drawingPosition);*/
-        }
-
-
-        /// <summary>
-        /// Checks for free intercept targets (cover, neutral animal, pawn) along the trajectory.
-        /// </summary>
-        protected void DetermineImpactExactPosition()
-        {
-            Log.Message("DetermineImpactExactPosition");
-
-            // We split the trajectory into small segments of approximatively 1 cell size.
-            Vector3 trajectory = (this.destination - this.origin);
-            int numberOfSegments = (int)trajectory.magnitude;
-            Vector3 trajectorySegment = (trajectory / trajectory.magnitude);
-
-            Vector3 lastValidPosition = this.origin; // Last valid tested position in case of an out of boundaries shot.
-            Vector3 nextPosition = this.origin;
-            IntVec3 testedPosition = nextPosition.ToIntVec3();
-
-            for (int segmentIndex = 1; segmentIndex <= numberOfSegments; segmentIndex++)
-            {
-
-                nextPosition += trajectorySegment;
-                testedPosition = nextPosition.ToIntVec3();
-
-                if (!nextPosition.InBounds(this.Map))
-                {
-                    this.destination = lastValidPosition;
-                    break;
-                }
-
-                if (CheckForFreeInterceptBetween(lastValidPosition, nextPosition))
-                {
-                    Log.Message("Impact between " + lastValidPosition + " and " + nextPosition);
-                    this.destination = nextPosition;
-                    return;
-                }
-                lastValidPosition = nextPosition;
-            }
-        }
-        
-        private static List<IntVec3> checkedCells = new List<IntVec3>();
-        private bool CheckForFreeInterceptBetween(Vector3 lastExactPos, Vector3 newExactPos)
-        {
-            IntVec3 intVec = lastExactPos.ToIntVec3();
-            IntVec3 intVec2 = newExactPos.ToIntVec3();
-            if (intVec2 == intVec)
-            {
-                return false;
-            }
-            if (!intVec.InBounds(base.Map) || !intVec2.InBounds(base.Map))
-            {
-                return false;
-            }
-            if (intVec2.AdjacentToCardinal(intVec))
-            {
-                return CheckForFreeIntercept(intVec2);
-            }
-            if (VerbUtility.InterceptChanceFactorFromDistance(origin, intVec2) <= 0f)
-            {
-                return false;
-            }
-            Vector3 vect = lastExactPos;
-            Vector3 v = newExactPos - lastExactPos;
-            Vector3 vector = v.normalized * 0.2f;
-            int num = (int)(v.MagnitudeHorizontal() / 0.2f);
-            checkedCells.Clear();
-            int num2 = 0;
-            IntVec3 intVec3;
-            do
-            {
-                vect += vector;
-                intVec3 = vect.ToIntVec3();
-                if (!checkedCells.Contains(intVec3))
-                {
-                    if (CheckForFreeIntercept(intVec3))
-                    {
-                        return true;
-                    }
-                    checkedCells.Add(intVec3);
-                }
-                num2++;
-                if (num2 > num)
-                {
-                    return false;
-                }
-            }
-            while (!(intVec3 == intVec2));
-            return false;
-        }
-
-        private bool CheckForFreeIntercept(IntVec3 c)
-        {
-            if (destination.ToIntVec3() == c)
-            {
-                return false;
-            }
-            float num = VerbUtility.InterceptChanceFactorFromDistance(origin, c);
-            if (num <= 0f)
-            {
-                return false;
-            }
-            bool flag = false;
-            List<Thing> thingList = c.GetThingList(base.Map);
-            for (int i = 0; i < thingList.Count; i++)
-            {
-                Thing thing = thingList[i];
-                if (!CanHit(thing))
-                {
-                    continue;
-                }
-                bool flag2 = false;
-                if (thing.def.Fillage == FillCategory.Full)
-                {
-                    Building_Door building_Door = thing as Building_Door;
-                    if (building_Door == null || !building_Door.Open)
-                    {
-                        ThrowDebugText("int-wall", c);
-                        Impact(thing);
-                        return true;
-                    }
-                    flag2 = true;
-                }
-                float num2 = 0f;
-                Pawn pawn = thing as Pawn;
-                if (pawn != null)
-                {
-                    num2 = 0.4f * Mathf.Clamp(pawn.BodySize, 0.1f, 2f);
-                    if (pawn.GetPosture() != 0)
-                    {
-                        num2 *= 0.1f;
-                    }
-                    if (launcher != null && pawn.Faction != null && launcher.Faction != null && !pawn.Faction.HostileTo(launcher.Faction))
-                    {
-                        num2 *= 0.4f;
-                    }
-                }
-                else if (thing.def.fillPercent > 0.2f)
-                {
-                    num2 = (flag2 ? 0.05f : ((!DestinationCell.AdjacentTo8Way(c)) ? (thing.def.fillPercent * 0.15f) : (thing.def.fillPercent * 1f)));
-                }
-                num2 *= num;
-                if (num2 > 1E-05f)
-                {
-                    if (Rand.Chance(num2))
-                    {
-                        ThrowDebugText("int-" + num2.ToStringPercent(), c);
-                        Impact(thing);
-                        return true;
-                    }
-                    flag = true;
-                    ThrowDebugText(num2.ToStringPercent(), c);
-                }
-            }
-            if (!flag)
-            {
-                ThrowDebugText("o", c);
-            }
-            return false;
-        }
-
-        private void ThrowDebugText(string text, IntVec3 c)
-        {
-            if (DebugViewSettings.drawShooting)
-            {
-                MoteMaker.ThrowText(c.ToVector3Shifted(), base.Map, text);
+                base.Tick();
             }
         }
 
@@ -329,31 +54,26 @@ namespace Weapons
                     pawn.stances.StaggerFor(95);
                 }
             }
-        }
-        
-        // ===================== Draw =====================
-        public void ComputeDrawingParameters()
-        {
-            if (this.tickCounter <= this.additionalParameters.preFiringDuration)
+
+            // Spawn projectile drawer.
+            Vector3 impactPosition;
+            if (hitThing != null)
             {
-                if (this.additionalParameters.preFiringDuration > 0)
-                {
-                    this.drawingIntensity = preFiringInitialIntensity + (preFiringFinalIntensity - preFiringInitialIntensity) * (float)this.tickCounter / (float)this.additionalParameters.preFiringDuration;
-                }
+                impactPosition = new Vector3(hitThing.DrawPos.x, 0f, hitThing.DrawPos.z);
             }
             else
             {
-                if (this.additionalParameters.postFiringDuration > 0)
-                {
-                    this.drawingIntensity = postFiringInitialIntensity + (postFiringFinalIntensity - postFiringInitialIntensity) * (((float)this.tickCounter - (float)this.additionalParameters.preFiringDuration) / (float)this.additionalParameters.postFiringDuration);
-                }
+                impactPosition = new Vector3(ExactPosition.x, 0f, ExactPosition.z);
+                MoteMaker.ThrowMicroSparks(impactPosition, map);
             }
+            Projectile_LaserDrawer projectileDrawer = ThingMaker.MakeThing(ThingDef.Named("LaserRay")) as Projectile_LaserDrawer;
+            projectileDrawer.Initialize(this.def, new Vector3(this.origin.x, 0f, this.origin.z), impactPosition);
+            GenSpawn.Spawn(projectileDrawer, (impactPosition - this.origin).ToIntVec3(), map);
         }
-        
+
         public override void Draw()
         {
-            Graphics.DrawMesh(MeshPool.plane10, this.drawingMatrix, FadedMaterialPool.FadedVersionOf(this.def.DrawMatSingle, this.drawingIntensity), 0);
-            Comps_PostDraw();
+            // Do nothing.
         }
     }
 }
---
+++ MiningCo. Weapons/Weapons/Projectile_LaserDrawer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;   // Always needed
+//using VerseBase;   // Material/Graphics handling functions are found here
+using RimWorld;      // RimWorld specific functions are found here
+using Verse;         // RimWorld universal objects are here
+//using Verse.AI;    // Needed when you do something with the AI
+//using Verse.Sound; // Needed when you do something with the Sound
+
+namespace Weapons
+{
+    /// <summary>
+    /// Projectile_LaserDrawer class.
+    /// </summary>
+    /// <author>Rikiki</author>
+    /// <permission>Use this code as you want, just remember to add a link to the corresponding Ludeon forum mod release thread.</permission>
+    public class Projectile_LaserDrawer : Thing
+    {
+        // Variables.
+        public int tickCounter = 0;
+        ThingDef projectileDef = null;
+        Vector3 origin;
+        Vector3 destination;
+
+        // Draw variables.
+        public const float preFiringInitialIntensity = 0f;
+        public const float preFiringFinalIntensity = 1f;
+        public const float postFiringInitialIntensity = 1f;
+        public const float postFiringFinalIntensity = 0f;
+        public Matrix4x4 drawingMatrix = default(Matrix4x4);
+        public Vector3 drawingScale;
+        public Vector3 drawingPosition;
+        public float drawingIntensity = 0f;
+        public Material drawingTexture = null;
+        public float exactRotation = 0f;
+
+        ThingDef_LaserProjectile additionalParameters
+        {
+            get
+            {
+                return this.projectileDef as ThingDef_LaserProjectile;
+            }
+        }
+        
+        // ===================== Setup Work =====================
+        public void Initialize(ThingDef projectileDef, Vector3 origin, Vector3 destination)
+        {
+            this.projectileDef = projectileDef;
+            this.origin = origin;
+            this.destination = destination;
+
+            InitializeDrawingParameters();
+        }
+
+        /// <summary>
+        /// Save and load internal state variables (stored in savegame data).
+        /// </summary>
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<int>(ref this.tickCounter, "tickCounter");
+            Scribe_Defs.Look<ThingDef>(ref this.projectileDef, "projectileDef");
+            Scribe_Values.Look<Vector3>(ref this.origin, "origin");
+            Scribe_Values.Look<Vector3>(ref this.destination, "destination");
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                InitializeDrawingParameters();
+            }
+        }
+        
+        public void InitializeDrawingParameters()
+        {
+            Vector3 laserRayVector = this.destination - this.origin;
+            Vector3 cannonMouthPositionOffset = laserRayVector.normalized * 0.52f;
+            this.drawingScale = new Vector3(1f, 1f, laserRayVector.magnitude - cannonMouthPositionOffset.magnitude);
+            this.drawingPosition = this.origin + cannonMouthPositionOffset + laserRayVector / 2f + Vector3.up * Altitudes.AltitudeFor(AltitudeLayer.Projectile);
+            this.exactRotation = laserRayVector.AngleFlat();
+            this.drawingMatrix.SetTRS(this.drawingPosition, laserRayVector.AngleFlat().ToQuat(), this.drawingScale);
+            this.Position = this.drawingPosition.ToIntVec3();
+        }
+
+        public override void PostMapInit()
+        {
+            base.PostMapInit();
+        }
+
+        // ===================== Main Work Function =====================
+        /// <summary>
+        /// Main projectile sequence.
+        /// </summary>
+        public override void Tick()
+        {
+            if (this.tickCounter <= this.additionalParameters.preFiringDuration)
+            {
+                if (this.additionalParameters.preFiringDuration > 0)
+                {
+                    this.drawingIntensity = preFiringInitialIntensity + (preFiringFinalIntensity - preFiringInitialIntensity) * (float)this.tickCounter / (float)this.additionalParameters.preFiringDuration;
+                }
+            }
+            else
+            {
+                if (this.additionalParameters.postFiringDuration > 0)
+                {
+                    this.drawingIntensity = postFiringInitialIntensity + (postFiringFinalIntensity - postFiringInitialIntensity) * (((float)this.tickCounter - (float)this.additionalParameters.preFiringDuration) / (float)this.additionalParameters.postFiringDuration);
+                }
+            }
+            this.tickCounter++;
+            if (this.tickCounter >= this.additionalParameters.preFiringDuration + this.additionalParameters.postFiringDuration)
+            {
+                this.Destroy();
+            }
+        }
+        
+        public override void Draw()
+        {
+            if (this.tickCounter == 0)
+            {
+                // Avoid drawing laser ray before it is initialized (this can happen when Draw is called before Tick).
+                return;
+            }
+            Log.Message("this.tickCounter = " + this.tickCounter + ", this.drawingScale = " + this.drawingScale + ", this.drawingPosition = " + this.drawingPosition);
+            Graphics.DrawMesh(MeshPool.plane10, this.drawingMatrix, FadedMaterialPool.FadedVersionOf(this.projectileDef.DrawMatSingle, this.drawingIntensity), 0);
+        }
+    }
+}
